f0e1f5824e1f565dee59f3292c505d06c4bdac29
Diogo Moura <diogo@stormsys.net>
2022-02-15 08:52:41 +0000

    no referer

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
         <div className="center-screen">
           <h1 className="name">Diogo Moura</h1>
           <div className="sub-text">
-            <a href="https://uk.linkedin.com/in/diogomoura1" target="_blank" alt="linked in">
+            <a href="https://uk.linkedin.com/in/diogomoura1" rel="noreferrer" target="_blank" alt="linked in">
               <i className="fa fa-linkedin-square fa-fw"/>
             </a>
-            <a href="https://github.com/stormsys" target="_blank" alt="github">
+            <a href="https://github.com/stormsys" rel="noreferrer" target="_blank" alt="github">
               <i className="fa fa-github-square fa-fw"/>
             </a>
-            <a href="/cv" target="_blank" alt="CV">
+            <a href="/cv" rel="noreferrer" target="_blank" alt="CV">
               <i className="fa fa-envelope-square fa-fw"/>
             </a>
 
